b9bef957945881908a63de37d41876ff4a58d366
Marcelo <marcheloomeneses1@gmail.com>
2024-02-25 13:02:32 -0300

    Add Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <footer className="flex items-center justify-center">
+      <a
+        href="https://github.com/Lfkoshe4rt/temporal-files-client"
+        target="_blank"
+        className="pb-6 text-violet-700 hover:text-violet-600"
+      >
+        &copy; Lfkoshe4rt
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
