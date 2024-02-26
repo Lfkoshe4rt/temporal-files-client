49b273c1c21a1168bb5f48ca804e232132ebf168
Marcelo <marcheloomeneses1@gmail.com>
2024-02-26 04:45:48 -0300

    Add Timer component

---
+++ src/components/Timer.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { useEffect, useState } from "react";
+import { TTimer } from "@/types/timer";
+
+const Timer = ({ time, onFinish, className }: TTimer) => {
+  const [minutes, setMinutes] = useState(time);
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (seconds > 0) {
+        setSeconds(seconds - 1);
+      } else if (minutes > 0) {
+        setMinutes(minutes - 1);
+        setSeconds(59);
+      } else {
+        clearInterval(timer);
+        onFinish();
+      }
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [minutes, seconds, onFinish]);
+
+  return (
+    <span className={className}>{`${minutes}:${
+      seconds < 10 ? "0" : ""
+    }${seconds}`}</span>
+  );
+};
+
+export default Timer;
