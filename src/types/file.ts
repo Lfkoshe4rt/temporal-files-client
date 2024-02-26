23e7b8a3173a3edbfa604a723a69506bcf534244
Marcelo <marcheloomeneses1@gmail.com>
2024-02-25 23:06:54 -0300

    Add TFile type definition

---
+++ src/types/file.ts
@@ -0,0 +1,11 @@
+export type TFile = {
+  _id: string;
+  name: string;
+  size: string;
+  url: string;
+  permanent: boolean;
+  private: boolean;
+  createdAt: string;
+  time: number;
+  key: string;
+};
