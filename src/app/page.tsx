fc6d575ec544b72b79a058895667b822238bee42
Marcelo <marcheloomeneses1@gmail.com>
2024-02-29 05:28:27 -0300

    Remove unused components from Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,9 @@
-import Header from "@/components/Header";
 import UploadFile from "@/components/UploadFile";
-import PreFooter from "@/components/PreFooter";
-import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <main className="mx-auto h-screen p-6 md:w-[1400px]">
-      <Header />
+    <main>
       <UploadFile />
-      <PreFooter />
-      <Footer />
     </main>
   );
 }
