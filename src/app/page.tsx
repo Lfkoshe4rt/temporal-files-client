c03714da56d25886873f1bd159c1fd3db1ffde69
Marcelo <marcheloomeneses1@gmail.com>
2024-03-02 02:06:10 -0300

    Refactor page component to include About, FileUploadForm, and Features components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-import UploadFile from "@/components/UploadFile";
+import About from "@/components/About";
+import Features from "@/components/Features";
+import FileUploadForm from "@/components/FileUploadForm";
 
 export default function Home() {
   return (
-    <main>
-      <UploadFile />
-    </main>
+    <>
+      <header>
+        <h1 className="text-balance text-center text-3xl p-6 font-bold sm:text-5xl">
+          Comparte archivos de manera temporal
+        </h1>
+      </header>
+
+      <About />
+      <main>
+        <FileUploadForm />
+      </main>
+      <Features />
+    </>
   );
 }
