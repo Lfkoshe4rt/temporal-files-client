21e4c1a46bea58fe43de41e9387a391d71674ec4
Marcelo <marcheloomeneses1@gmail.com>
2024-03-02 06:42:08 -0300

    Delete Store component and related functions

--- src/components/Store.tsx
+++
@@ -1,8 +0,0 @@
-import { TFile } from "@/types/file";
-import { atom } from "nanostores";
-
-export const files = atom<TFile[]>([]);
-
-export const addFile = (file: TFile) => {
-  files.set([...files.get(), file]);
-};
