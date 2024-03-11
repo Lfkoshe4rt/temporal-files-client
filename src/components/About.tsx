362a091476a908c9e6b9281c0f4bcc0535ee16be
Marcelo <marcheloomeneses1@gmail.com>
2024-03-10 21:13:18 -0300

    Update About component styling

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,11 +1,11 @@
 const About = () => {
   return (
     <section className="m-auto flex flex-col items-center justify-center pb-24">
-      <div className="m-auto flex flex-col items-center justify-center gap-6 pt-8 md:flex-row md:gap-20 md:pt-12">
+      <div className="px-4 m-auto flex flex-col items-center justify-center gap-6 pt-8 md:flex-row md:gap-20 md:pt-12">
         <p className="text-balance leading-loose tracking-wide md:w-[390px]">
-          <span className="sm;inline-block text-base font-bold md:tracking-wide">
-            ¿Te preguntas para qué podría ser útil esto?
-          </span>{" "}
+          <span className="text-center text-base font-bold md:tracking-wide">
+            ¿Te preguntas para qué podría ser útil esto?&nbsp;
+          </span>
           Bueno, imagina que necesitas compartir un archivo con alguien por un
           tiempo limitado, digamos un informe confidencial con un colega o unas
           fotos de un evento que quieres que tus amigos vean durante un período
@@ -15,7 +15,8 @@ const About = () => {
 
         <p className="justify-center text-balance leading-loose tracking-wide md:w-[390px]">
           <span className="font-bold">
-            Te brindamos la opción de gestionar la privacidad de tus archivos.
+            Te brindamos la opción de gestionar la privacidad de tus
+            archivos.&nbsp;
           </span>
           Puedes establecerlos como públicos, lo que permite el acceso a
           cualquier persona con el enlace durante el período designado, o como
