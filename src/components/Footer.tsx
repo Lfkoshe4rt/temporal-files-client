26700f675c1c59f6807558a5b55ce48000949ecf
Marcelo <marcheloomeneses1@gmail.com>
2024-03-13 07:58:27 -0300

    Update link color in Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
         href="https://github.com/Lfkoshe4rt/temporal-files-client"
         target="_blank"
         rel="noreferrer noopener"
-        className="pb-6 text-violet-700 hover:text-violet-600"
+        className="pb-6 text-green-700 hover:text-green-600"
       >
         &copy; Lfkoshe4rt
       </a>
