9001a4d976c21959c00f41cb32a4423de9b49350
Marcelo <marcheloomeneses1@gmail.com>
2024-03-30 04:45:07 -0300

    Add formatDate function to format dates

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,9 +1,18 @@
 export const convertToUnits = (bytes: number): string => {
-	if (bytes === 0) return '0 Bytes'
+  if (bytes === 0) return "0 Bytes";
 
-	const k = 1024
-	const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB']
-	const i = Math.floor(Math.log(bytes) / Math.log(k))
+  const k = 1024;
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
 
-	return `${(bytes / Math.pow(k, i)).toFixed(2)} ${sizes[i]}`
-}
+  return `${(bytes / Math.pow(k, i)).toFixed(2)} ${sizes[i]}`;
+};
+
+export const formateDate = (date: string): string => {
+  const d = new Date(date);
+
+  const day = d.getDate().toString().padStart(2, "0");
+  const month = (d.getMonth() + 1).toString().padStart(2, "0");
+
+  return `${day}/${month}/${d.getFullYear()}`;
+};
