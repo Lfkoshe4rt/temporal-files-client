8d533097cc03ab75b0058e69945ce01359b13d40
Marcelo <marcheloomeneses1@gmail.com>
2024-03-29 23:58:05 -0300

    Refactor file page component

--- src/app/file/[id]/page.tsx
+++ src/app/file/[id]/page.tsx
@@ -1,13 +1,7 @@
 import Header from "@/components/Header";
-import Timer from "@/components/Timer";
+import PrivateFile from "@/components/PrivateFile";
 import { TResponse } from "@/types/file";
-import { redirect } from "next/navigation";
-
-import { convertToUnits } from "@/utils/format";
-
-enum ErrorFile {
-  FAILED = "FAILED",
-}
+import FileInfo from "@/components/FileInfo";
 
 export default async function File({ params }: { params: { id: string } }) {
   const { id } = params;
@@ -16,46 +10,13 @@ export default async function File({ params }: { params: { id: string } }) {
     cache: "no-cache",
   }).then((res) => res.json());
 
-  if (response.status == ErrorFile.FAILED) redirect("/");
-
-  const { data } = response;
+  const { data, message } = response;
 
   return (
     <>
       <Header />
 
-      <main>
-        <ul className="list-none">
-          <li>
-            id : <span>{data._id}</span>
-          </li>
-          <li>
-            <h1>Nombre: {data.name}</h1>
-          </li>
-          <li>Tamaño: {convertToUnits(Number(data.size))}</li>
-          <li>Permanente : {data.permanent ? "Si" : "No"}</li>
-          <li>Privado : {data.private ? "Si" : "No"}</li>
-          <li>Creado: {new Date(data.createdAt).toLocaleDateString()}</li>
-          <li>
-            <span>
-              Expira en:{" "}
-              {
-                <Timer
-                  createdAt={data.createdAt}
-                  className="text-yellow-300"
-                  min={data.time}
-                />
-              }
-              minutos
-            </span>
-          </li>
-          <li>
-            <a href={data.url} className="underline" download>
-              Descargar
-            </a>
-          </li>
-        </ul>
-      </main>
+      <main>{!data ? <PrivateFile id={id} /> : <FileInfo file={data} />}</main>
     </>
   );
 }
