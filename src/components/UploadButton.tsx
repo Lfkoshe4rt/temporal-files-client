8ba221bfdc3a0ec8da4835bd228bd2989636aaf0
Marcelo <marcheloomeneses1@gmail.com>
2024-04-05 15:59:41 -0300

    Refactor UploadButton component to use setPrepareFile instead of setFile for file handling

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -2,19 +2,21 @@ import { truncateString } from "@/utils/string";
 import { TUploadButton } from "@/types/uploadButton";
 import useIsMobile from "@/hooks/useIsMobile";
 
-const UploadButton = ({ setFile, file }: TUploadButton) => {
+const UploadButton = ({ setPrepareFile, prepareFile }: TUploadButton) => {
   const isMobile = useIsMobile();
 
   const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
     const droppedFile = e.dataTransfer?.files[0];
-    setFile(droppedFile);
+    setPrepareFile(droppedFile);
   };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const selectedFile = e.target.files?.[0];
 
-    setFile(selectedFile);
+    setPrepareFile(selectedFile);
+
+    e.target.value = "";
   };
 
   const handleDragOver: React.DragEventHandler<HTMLDivElement> = (e) => {
@@ -25,18 +27,18 @@ const UploadButton = ({ setFile, file }: TUploadButton) => {
     <section
       onDragOver={handleDragOver}
       onDrop={handleDrop}
-      className="flex justify-center items-center"
+      className="flex items-center justify-center"
     >
       <label
         htmlFor="file"
-        className="flex h-28 w-[300px] sm:w-[700px] cursor-pointer items-center justify-center rounded-md  border-8  border-dashed  border-white  font-bold  uppercase  text-white hover:border-slate-300  hover:text-slate-300"
+        className="flex h-28 w-[300px] cursor-pointer items-center justify-center rounded-md border-8  border-dashed  border-white  font-bold  uppercase  text-white  hover:border-slate-300 hover:text-slate-300  sm:w-[700px]"
       >
         <span className="px-3 text-center text-sm md:text-base">
-          {file
-            ? truncateString(file.name, 35, 35)
+          {prepareFile
+            ? truncateString(prepareFile.name, 35, 35)
             : isMobile
-            ? "Presiona para subir un archivo"
-            : "Arrastra y suelta un archivo aquí o presiona para subirlo"}
+              ? "Presiona para subir un archivo"
+              : "Arrastra y suelta un archivo aquí o presiona para subirlo"}
         </span>
       </label>
       <input
