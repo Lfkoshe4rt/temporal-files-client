e5f84c7765786e46dbf19fc2e0580507ac3cea09
Marcelo <marcheloomeneses1@gmail.com>
2024-04-06 05:20:48 -0300

    Refactor Modal component to improve file handling and UI

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,11 +1,13 @@
+import { TFile } from "@/types/file";
+
 const Modal = ({
   file,
   isOpen,
   onClose,
 }: {
-  file: any;
-  isOpen: any;
-  onClose: any;
+  file: TFile;
+  isOpen: boolean;
+  onClose: () => void;
 }) => {
   if (!isOpen) return null;
 
@@ -15,55 +17,77 @@ const Modal = ({
 
   return (
     <>
-      <div
-        onClick={onClose}
-        className="absolute right-0 top-0 h-full w-full bg-black opacity-70"
-      ></div>
-      <div className="fixed inset-0 z-10 flex  items-center justify-center overflow-y-auto ">
-        <div className="relative h-auto w-96 rounded-md bg-white">
-          <button
+      <div className="fixed inset-0 z-10 flex items-center justify-center overflow-y-auto">
+        <div className="fixed inset-0 transition-opacity" aria-hidden="true">
+          <div
+            className="absolute inset-0 bg-black opacity-75"
             onClick={onClose}
-            className=" absolute right-3 top-1 text-xl font-bold text-white hover:text-slate-200 "
-          >
-            X
-          </button>
-          <div className="flex h-32 items-center justify-center rounded-t-md bg-green-400">
-            <svg width="84" height="84" viewBox="0 0 24 24" fill="currentColor">
-              <path
-                d="M10.2426 16.3137L6 12.071L7.41421 10.6568L10.2426 13.4853L15.8995 7.8284L17.3137 9.24262L10.2426 16.3137Z"
+          ></div>
+        </div>
+
+        <div className="inline-block transform overflow-hidden rounded-lg bg-white text-left align-bottom shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:align-middle">
+          <div className="bg-white ">
+            <div className="flex h-36 items-center justify-center bg-green-400 ">
+              <svg
+                width="84"
+                height="84"
+                viewBox="0 0 24 24"
                 fill="currentColor"
-              />
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12ZM12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21Z"
-                fill="currentColor"
-              />
-            </svg>
-          </div>
-          <div className="flex h-auto flex-col items-center justify-center gap-3 p-3 text-black">
-            <div className="text-center text-slate-600 ">
+              >
+                <path
+                  d="M10.2426 16.3137L6 12.071L7.41421 10.6568L10.2426 13.4853L15.8995 7.8284L17.3137 9.24262L10.2426 16.3137Z"
+                  fill="currentColor"
+                />
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12ZM12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21Z"
+                  fill="currentColor"
+                />
+              </svg>
+            </div>
+            <div className="pb-5 text-center text-xl text-slate-600">
               <h1 className="text-3xl ">Suceso!</h1>
               <p className="p-1">Archivo subido correctamente</p>
 
               {file.private && (
-                <p
-                  onClick={handleCopyKey}
-                  title="CLICK PARA COPIAR LA CLAVE EN EL PORTAPAPELES"
-                >
-                  Clave: &nbsp;
-                  <span className="cursor-pointer text-green-700 hover:text-green-500">
-                    {file.key}
-                  </span>
-                </p>
+                <div className="flex items-center justify-center gap-2">
+                  <p>Clave:</p>
+                  <button
+                    onClick={handleCopyKey}
+                    className="text-green-600 hover:text-green-700"
+                    title="Copiar clave"
+                  >
+                    <span className="flex items-center gap-1">
+                      <svg
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                      >
+                        <path d="M13 7H7V5H13V7Z" fill="currentColor" />
+                        <path d="M13 11H7V9H13V11Z" fill="currentColor" />
+                        <path d="M7 15H13V13H7V15Z" fill="currentColor" />
+                        <path
+                          fillRule="evenodd"
+                          clipRule="evenodd"
+                          d="M3 19V1H17V5H21V23H7V19H3ZM15 17V3H5V17H15ZM17 7V19H9V21H19V7H17Z"
+                          fill="currentColor"
+                        />
+                      </svg>{" "}
+                      {file.key}
+                    </span>
+                  </button>
+                </div>
               )}
+
+              <a
+                href={`/file/${file._id}`}
+                className="mt-2 inline-block rounded-md bg-green-400 px-12 py-2 text-white transition-colors duration-300 ease-in-out hover:bg-green-500"
+              >
+                Ir al archivo
+              </a>
             </div>
-            <a
-              href={`/file/${file._id}`}
-              className="rounded-md bg-green-400 px-12 py-2 text-white transition-colors duration-300 ease-in-out hover:bg-green-500"
-            >
-              Ir al archivo
-            </a>
           </div>
         </div>
       </div>
