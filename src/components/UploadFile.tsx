91a8de6151fb5305a80052b54b7677108f87df92
Marcelo <marcheloomeneses1@gmail.com>
2024-04-08 22:05:48 -0300

    Add framer-motion animation to UploadFile component in UploadFile.tsx

--- src/components/UploadFile.tsx
+++ src/components/UploadFile.tsx
@@ -1,10 +1,17 @@
 "use client";
 import FileUploadLogic from "@/hooks/FileUploadLogic";
+import { motion, AnimatePresence } from "framer-motion";
 
 import FileControlButtons from "./FileControlButtons";
 import FileOptions from "./FileOptions";
 import Modal from "./Modal";
 import ButtonUpload from "./UploadButton";
+
+const variants = {
+  initial: { scale: 0.5, opacity: 0 },
+  animate: { scale: 1, opacity: 1 },
+  exit: { scale: 0.5, opacity: 0 },
+};
 
 const UploadFile = () => {
   const {
@@ -24,13 +31,25 @@ const UploadFile = () => {
         <ButtonUpload
           setPrepareFile={setPrepareFile}
           prepareFile={prepareFile!}
-        />
-        {prepareFile && (
-          <>
-            <FileOptions file={prepareFile} />
-            <FileControlButtons handleRemove={handleRemove} loading={loading} />
-          </>
-        )}
+        />{" "}
+        <AnimatePresence>
+          {prepareFile && (
+            <motion.div
+              initial="initial"
+              animate="animate"
+              exit="exit"
+              transition={{ duration: 0.2 }}
+              variants={variants}
+              key="file-options"
+            >
+              <FileOptions file={prepareFile!} />
+              <FileControlButtons
+                handleRemove={handleRemove}
+                loading={loading}
+              />
+            </motion.div>
+          )}
+        </AnimatePresence>
       </form>
 
       <Modal isOpen={isOpen} onClose={toggleModal} file={file!} />
