87f80ea7d8acbd0fb8108200048e4baba2791c93
Marcelo <marcheloomeneses1@gmail.com>
2024-04-15 04:35:28 -0300

    Add FAQsection component and update page layout in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,23 @@
 import About from "@/components/About";
 import Features from "@/components/Features";
 import UploadFile from "@/components/UploadFile";
+import FAQsection from "@/components/FAQsection";
 
 export default function Home() {
   return (
     <>
       <header>
-        <h1 className="text-balance text-center text-3xl p-6 font-bold sm:text-5xl">
+        <h1 className="text-balance p-6 text-center text-3xl font-bold sm:text-5xl">
           Comparte archivos de manera temporal
         </h1>
       </header>
+      <About />
       <main>
-        <About />
-
         <UploadFile />
-
-        <Features />
       </main>
+
+      <FAQsection />
+      <Features />
     </>
   );
 }
