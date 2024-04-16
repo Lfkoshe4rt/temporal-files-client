9c6bfb0ce9386c48db60c2003dbb7ae2322b6a94
Marcelo <marcheloomeneses1@gmail.com>
2024-04-16 03:11:59 -0300

    Update Header component with responsive styling and mobile menu in Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,28 @@
 const Header = () => {
   return (
     <header>
-      <nav className="h-12  flex items-center justify-between w-full px-12">
+      <nav className="mt-3 flex h-12 w-full items-center justify-between px-2 sm:px-12">
         <a href="/" className="text-xl font-bold uppercase">
           Temporal files
         </a>
-        <ul className="flex gap-5">
+        <button className="sm:hidden">
+          <svg
+            className="h-6 w-6 sm:hidden"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16m-7 6h7"
+            ></path>
+          </svg>
+        </button>
+
+        <ul className="hidden gap-5 sm:flex">
           <li>
             <a href="/">Inicio</a>
           </li>
