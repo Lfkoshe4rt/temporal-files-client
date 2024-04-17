d4a8ca494819778a0c6ec0ec0dd88350e78c6d45
Marcelo <marcheloomeneses1@gmail.com>
2024-04-17 04:01:02 -0300

    Update Header component with responsive styling and mobile menu in Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,35 +1,40 @@
+"use client";
+
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import Menu from "./Menu";
+import ButtonMobile from "./ButtonMobile";
+
 const Header = () => {
+  const [open, setOpen] = useState(false);
+
+  const toggleMenu = () => setOpen(!open);
+
   return (
     <header>
       <nav className="mt-3 flex h-12 w-full items-center justify-between px-2 sm:px-12">
-        <a href="/" className="text-xl font-bold uppercase">
+        <a href="/" className="z-10 text-xl font-bold uppercase">
           Temporal files
         </a>
-        <button className="sm:hidden">
-          <svg
-            className="h-6 w-6 sm:hidden"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16m-7 6h7"
-            ></path>
-          </svg>
-        </button>
 
-        <ul className="hidden gap-5 sm:flex">
-          <li>
-            <a href="/">Inicio</a>
-          </li>
-          <li>
-            <a href="/contacto">Contacto</a>
-          </li>
-        </ul>
+        <ButtonMobile open={open} onClick={toggleMenu} />
+
+        <Menu className="hidden gap-5 sm:flex" />
+
+        <AnimatePresence>
+          {open && (
+            <motion.div
+              initial={{ x: -600 }}
+              animate={{ x: 0 }}
+              exit={{ x: -600 }}
+              transition={{ duration: 0.3 }}
+              className="fixed inset-0 flex items-center justify-center bg-black opacity-90"
+              key="menu-mobile"
+            >
+              <Menu className="flex flex-col" />
+            </motion.div>
+          )}
+        </AnimatePresence>
       </nav>
     </header>
   );
