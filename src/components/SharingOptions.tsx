e5b43c47ca9b0f00cf3c3774ba45dc90dfead198
Marcelo <marcheloomeneses1@gmail.com>
2024-04-22 19:46:54 -0300

    Update SharingOptions component with new hover effects

--- src/components/SharingOptions.tsx
+++ src/components/SharingOptions.tsx
@@ -25,6 +25,7 @@ const SharingOptions = ({ id }: { id: string }) => {
             strokeWidth="2"
             strokeLinecap="round"
             strokeLinejoin="round"
+            className="transition-transform duration-300 ease-in-out hover:scale-110 hover:text-slate-300"
           >
             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
             <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
@@ -45,6 +46,7 @@ const SharingOptions = ({ id }: { id: string }) => {
             strokeWidth="2"
             strokeLinecap="round"
             strokeLinejoin="round"
+            className="transition-transform duration-300 ease-in-out hover:scale-110 hover:text-slate-300"
           >
             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
             <path d="M4 4l11.733 16h4.267l-11.733 -16z" />
@@ -64,6 +66,7 @@ const SharingOptions = ({ id }: { id: string }) => {
             strokeWidth="2"
             strokeLinecap="round"
             strokeLinejoin="round"
+            className="transition-transform duration-300 ease-in-out hover:scale-110 hover:text-slate-300"
           >
             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
             <path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z" />
@@ -81,6 +84,7 @@ const SharingOptions = ({ id }: { id: string }) => {
             strokeWidth="2"
             strokeLinecap="round"
             strokeLinejoin="round"
+            className="transition-transform duration-300 ease-in-out hover:scale-110 hover:text-slate-300"
           >
             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
             <path d="M9 15l6 -6" />
